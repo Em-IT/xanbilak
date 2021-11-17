28e01f54f67ed6810127752f268cafbeca7cd3b0
EmIT <emad.armoun@gmail.com>
2021-11-17 18:59:35 +0330

    feat: define custom color & font

--- xanbilak-fe/tailwind.config.js
+++ xanbilak-fe/tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myviolet: {
+          light: '#9f2aed',
+          DEFAULT: "#8027b8",
+          dark: '#5e0e93',
+        },
+      },
+      fontFamily: {
+        'body': ['"Vazir"', '"Open Sans"'],
+       }
+    },
   },
   variants: {
     extend: {},
