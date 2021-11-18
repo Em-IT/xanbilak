367504b3ed13e5012c90829a15fb9a49474f4273
EmIT <emad.armoun@gmail.com>
2021-11-18 11:50:12 +0330

    refactor: convert navbar styles to tailwind

--- xanbilak-fe/src/components/NavBar/NavBar.tsx
+++ xanbilak-fe/src/components/NavBar/NavBar.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import "./NavBar.scss";
 import { IoMdSettings } from "react-icons/io";
 import { TiMessages } from "react-icons/ti";
 import { FaListOl } from "react-icons/fa";
@@ -12,23 +11,23 @@ const NavBar = (): JSX.Element => {
   const { t } = useTranslation();
 
   return (
-    <div className="navbar">
-      <Link to={"/"}>
-        <div>
+    <div className="flex flex-row sticky bottom-0 w-full bg-gradient-to-b from-white to-gray-300" style={{height: '75px'}}>
+      <Link to={"/"} className="w-1/3 no-underline">
+        <div className="flex flex-col justify-center items-center text-gray-700 hover:text-red-700">
           <span>{t("shopping_list")}</span>
-          <FaListOl className="list-icon" />
+          <FaListOl className="hover:text-red-700 w-5 h-5" />
         </div>
       </Link>
-      <Link to={"/messenger"}>
-        <div>
+      <Link to={"/messenger"} className="w-1/3 no-underline">
+        <div className="flex flex-col justify-center items-center text-gray-700 hover:text-blue-700">
           <span>{t("messenger")}</span>
-          <TiMessages className="messenger-icon" />
+          <TiMessages className="text-blue-700 w-6 h-6" />
         </div>
       </Link>
-      <Link to={"/settings"}>
-        <div>
+      <Link to={"/settings"} className="w-1/3 no-underline">
+        <div className="flex flex-col justify-center items-center text-gray-700 hover:text-green-700">
           <span>{t("settings")}</span>
-          <IoMdSettings className="setting-icon" />
+          <IoMdSettings className="text-green-700 w-5 h-5" />
         </div>
       </Link>
     </div>
