a7a8cc0aa144a020ccc4b4a8ef8c5d30537b9534
EmIT <emad.armoun@gmail.com>
2021-11-18 11:50:32 +0330

    refactor: convert main header styles to tw

--- xanbilak-fe/src/components/MainHeader/MainHeader.tsx
+++ xanbilak-fe/src/components/MainHeader/MainHeader.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import "./MainHeader.scss";
 import whiteLogo from "../../assets/images/logo-white.png";
 
 const MainHeader = (): JSX.Element => (
-  <div className="main-header">
-    <span className="title">anbilak</span>
-    <img src={whiteLogo} alt="Xanbilak Logo" className="xanbilak-logo" />
+  <div className="flex flex-row justify-center items-center h-10 text-white bg-gradient-to-b from-my-violet-dark to-my-violet">
+    <span className="text-2xl font-bold" style={{fontFamily: "Times"}}>anbilak</span>
+    <img src={whiteLogo} alt="Xanbilak Logo" className="w-6" />
   </div>
 );
 
